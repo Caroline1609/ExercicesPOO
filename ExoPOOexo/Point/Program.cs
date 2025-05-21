dd9ad7128f44740442cd908275cbe7562c3c7e80
Caroline <70326049+Caroline1609@users.noreply.github.com>
2025-05-21 17:55:28 +0200

    upload

--- ExoPOOexo/Point/Program.cs
+++ ExoPOOexo/Point/Program.cs
@@ -8,7 +8,7 @@ namespace Point
     {
         static void Main(string[] args)
         {
-            PointYX p1 = new PointYX(8, 25);
+            PointYX p1 = new PointYX(21, 35);
 
             Console.WriteLine("Coordonnées initiales :");
             Console.WriteLine(p1.ToString());
@@ -18,10 +18,28 @@ namespace Point
             Console.WriteLine(p1.ToString());
 
 
+            PointYX symetrieAbscisse = p1.SymetrieAxeAbscisse();
+            Console.WriteLine("Symétrie par rapport à l'axe des abscisses :");
+            Console.WriteLine(symetrieAbscisse.ToString());
 
-            p1.SymetrieAxeAbscisse();
-            Console.WriteLine("Symetrie Axe Abscisse :");
-            Console.WriteLine(p1.ToString());
+            // Appliquer la symétrie par rapport à l'axe des ordonnées
+            PointYX symetrieOrdonnee = symetrieAbscisse.SymetrieAxeOrdonnee();
+            Console.WriteLine("Symétrie par rapport à l'axe des ordonnées :");
+            Console.WriteLine(symetrieOrdonnee.ToString());
+
+            // Appliquer la symétrie par rapport à l'origine
+            PointYX symetrieOrigine = symetrieOrdonnee.SymetrieOrigine();
+            Console.WriteLine("Symétrie par rapport à l'origine :");
+            Console.WriteLine(symetrieOrigine.ToString());
+
+
+
+
+            //PointYX symetrieCombinee = p1.SymetrieAxeAbscisse().SymetrieAxeOrdonnee().SymetrieOrigine();
+
+
+            //Console.WriteLine("Point après application des trois symétries :");
+            //Console.WriteLine(symetrieCombinee.ToString());
 
 
 
