25a321950c95c7b8cb2a7730a6c27d0ff4034e4d
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-05-21 16:54:08 +0200

    upload

--- ExoPOOexo/ClassLibraryPoint/Point.cs
+++ ExoPOOexo/ClassLibraryPoint/Point.cs
@@ -1,17 +1,76 @@
-﻿namespace ClassLibraryPoint
+﻿using System.Drawing;
+
+namespace ClassLibraryPoint
 {
-    public class Point
+    public class PointYX
     {
         private float y;
         private float x;
 
 
-        public Point (float x, float y)
+        public PointYX(float x, float y)
         {
             this.x = x;
             this.y = y;
 
         }
+
+        public float Y { get => y; }
+        public float X { get => x; }
+
+
+        public override string ToString()
+        {
+            return $"Le Point X : {x}\n et le point Y : {y}\n";
+        }
+
+        public void Deplacer(float _nouveauX, float _nouveauY)
+        {
+            this.x = _nouveauX;
+            this.y = _nouveauY;
+        }
+
+        public void Permuter()
+        {
+            float temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public PointYX SymetrieAxeAbscisse()
+        {
+            return new PointYX(x, -y);
+        }
+
+        public PointYX SymetrieAxeOrdonnee()
+        {
+            return new PointYX(-x, y);
+        }
+
+        public PointYX SymetrieOrigine()
+        {
+            return new PointYX(-x, -y);
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- ExoPOOexo/Point/Program.cs
+++ ExoPOOexo/Point/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Point
+﻿using ClassLibraryPoint;
+using System;
+using System.Drawing;
+
+namespace Point
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PointYX p1 = new PointYX(8, 25);
+
+            Console.WriteLine("Coordonnées initiales :");
+            Console.WriteLine(p1.ToString());
+
+            p1.Permuter();
+            Console.WriteLine("Après permutation :");
+            Console.WriteLine(p1.ToString());
+
+
+
+            p1.SymetrieAxeAbscisse();
+            Console.WriteLine("Symetrie Axe Abscisse :");
+            Console.WriteLine(p1.ToString());
+
+
+
+
+
+
+
+            Console.ReadLine();
+            
         }
     }
 }
