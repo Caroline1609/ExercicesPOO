003b1dbd27251cf96d1c70e3db55722534409b2e
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-05-23 11:38:19 +0200

    upload

---
+++ ExoPOOexo/ClassLibraryFraction/Fraction.cs
@@ -0,0 +1,170 @@
+﻿namespace ClassLibraryFraction
+{
+    public class Fraction
+    {
+        private int numerateur;
+        private int denominateur;
+
+
+        public int Numerateur { get => numerateur; }
+        public int Denominateur { get => denominateur; }
+
+
+        public Fraction() : this(1,2)
+        {
+
+        }
+
+        public Fraction(int numerateur, int denominateur)
+        {
+            this.numerateur = numerateur;
+            this.denominateur = denominateur;
+
+        }
+
+        public string ToString()
+        {
+            return $"{this.numerateur}/{denominateur}"; //a modifier
+        }  
+
+        public void Oppose()
+        {
+            this.numerateur = -this.numerateur;
+        }
+
+        public void Inverse()
+        {
+            int temp = this.numerateur;
+            this.numerateur = this.denominateur;
+            this.denominateur = temp;
+        }
+
+        private int getPgcd()
+        {
+            int a = this.numerateur;
+            int b = this.denominateur;
+            int pgcd = 1;
+
+            if (a != 0 && b != 0)
+            {
+                if (a < 0)
+                {
+                    a = -a;
+                }
+                if (b < 0)
+                {
+                    b = -b;
+                }
+                while (a != b)
+                {
+                    if (a < b)
+                    {
+                        b = b - a;
+                    }
+                    else
+                    {
+                        a = a - b;
+                    }
+                }
+                pgcd = a;
+            }
+
+            return pgcd;
+        }
+
+        public bool SuperieurA(Fraction autreFraction)
+        {
+            bool resultat;
+
+            if (this.numerateur * autreFraction.denominateur > autreFraction.numerateur * this.denominateur)
+            {
+                resultat = true;
+            }
+            else
+            {
+                resultat = false;
+            }
+            return resultat;
+        }
+
+        public bool EgalA(Fraction autreFraction)
+        {
+            bool resultat;
+
+            if (this.numerateur * autreFraction.denominateur == autreFraction.numerateur * this.denominateur)
+            {
+                resultat = true;
+            }
+            else
+            {
+                resultat = false;
+            }
+            return resultat;                   
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public Fraction(Fraction fractionACloner) : this(fractionACloner.numerateur, fractionACloner.denominateur)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    
+
+        
+
+        
+
+
+
+
+
+
+
+        //public string ToDisplay()
+        //{
+        //    return $"{numerateur}/{denominateur}";
+        //}
+
+
+}
+
--- ExoPOOexo/Fraction/Program.cs
+++ ExoPOOexo/Fraction/Program.cs
@@ -7,15 +7,26 @@ namespace Fraction
         static void Main(string[] args)
         {
 
-            Fractions f1 = new Fractions(2, 5);
-            Fractions f2 = new Fractions(3, 5);
+            ClassLibraryFraction.Fraction f1 = new ClassLibraryFraction.Fraction(11,7);
+            ClassLibraryFraction.Fraction f2 = new ClassLibraryFraction.Fraction(22, 14);
 
 
-            Console.WriteLine(f1.ToString());
-            Fractions quotient = f1.Divise(f2);
-            Console.WriteLine(f1.EgalA(f2));
+            Console.WriteLine($"Fraction 1 : {f1.ToString()}");
+            Console.WriteLine($"Fraction 2 : {f2.ToString()}");
+            //Fractions quotient = f1.Divise(f2);
 
 
+            //f1.Oppose();
+            //Console.WriteLine(f1.ToString());
+
+            //f1.Inverse();
+            //Console.WriteLine(f1.ToString());
+
+            bool estSuperieur = f1.SuperieurA(f2);
+            Console.WriteLine($"Supérieure: {estSuperieur}");
+
+            bool estEgal = f1.EgalA(f2);
+            Console.WriteLine($"Supérieure: {estEgal}");
 
 
 
