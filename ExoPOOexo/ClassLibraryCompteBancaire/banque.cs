7020f0448f792a00fb767bdbf35ff98b78502891
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-05-27 16:53:44 +0200

    upload

--- ExoPOOexo/ClassLibraryCompteBancaire/banque.cs
+++ ExoPOOexo/ClassLibraryCompteBancaire/banque.cs
@@ -9,5 +9,82 @@ namespace ClassLibraryCompteBancaire
     public class Banque
     {
         private List<CompteBancaire> mesComptes;
+
+        private string nom;
+        private string ville;
+
+        public List<CompteBancaire> MesComptes { get => mesComptes; }
+        public int NbComptes { get => mesComptes.Count; }
+        public string Nom { get => nom; }
+        public string Ville { get => ville; }
+
+
+        public Banque(Banque banqueACloner) : this(banqueACloner.mesComptes, banqueACloner.nom, banqueACloner.ville)
+        {
+
+        }
+
+
+        public Banque(List<CompteBancaire> mesComptes, string nom, string ville)
+        {
+            this.mesComptes = mesComptes;
+            this.nom = nom;
+            this.ville = ville;
+        }
+
+
+        public void AjouteCompte(int numero, string nom, decimal solde, int decouvertAutorise)
+        {
+            CompteBancaire ajouterCompte = new CompteBancaire(numero, nom, solde, decouvertAutorise);
+            AjouterCompte(ajouterCompte);
+        }
+
+
+        public void AjouterCompte(CompteBancaire ajouteCompte)
+        {
+            this.mesComptes.Add(ajouteCompte);
+        }
+
+
+        public CompteBancaire CompteSup()
+        { 
+            CompteBancaire compteMax = mesComptes[0];
+
+            foreach (var compte in mesComptes)
+            {
+                if (compte.Solde > compteMax.Solde)
+                {
+                    compteMax = compte;
+                }
+                    
+            }
+
+            return compteMax;
+        }
+
+        public CompteBancaire RendCompte(int numero)
+        {
+            foreach (var compte in mesComptes)
+            {
+                if (compte.Numero == numero)
+                {
+                    return compte;
+                }
+                    
+            }
+            return null;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- ExoPOOexo/ExoPOO/Program.cs
+++ ExoPOOexo/ExoPOO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ClassLibraryCompteBancaire;
+using System.Numerics;
 
 namespace ExoPOO
 {
@@ -7,10 +8,10 @@ namespace ExoPOO
         static void Main(string[] args)
         {
 
-            CompteBancaire compte1 = new CompteBancaire(25485245, "Bruno Le méchant", 1000.00m, -500);
-            CompteBancaire compte2 = new CompteBancaire(25485245, "Caroline", 600.00m, -1000);
+            //CompteBancaire compte1 = new CompteBancaire(25485245, "Bruno Le méchant", 1000.00m, -500);
+            //CompteBancaire compte2 = new CompteBancaire(25485245, "Caroline", 600.00m, -1000);
 
-
+                       
             //Console.WriteLine("\tEtat des comptes\n");
             //Console.WriteLine(compte2.ToString());
             //Console.WriteLine(compte1.ToString());
@@ -32,14 +33,29 @@ namespace ExoPOO
 
             //int c = compte1.CompareTo(compte2);
 
+
+
             //List<CompteBancaire> dreamTeam = new List<CompteBancaire>();
             //dreamTeam.Add(compte2);
             //dreamTeam.Add(compte1);
             //dreamTeam.Sort(new CompteBancaireComparaisonParId());
 
-           
+
+            Banque b1 = new Banque(new List<CompteBancaire>(), "Crédit Mutuel", "Mulhouse");
+
+            b1.AjouteCompte(15515, "Alice", 1000m, 500);
+            b1.AjouteCompte(25185, "Bob", 2000m, 1000);
+            b1.AjouteCompte(26854, "Bruno Méchant", 100m, 500);
+            b1.AjouteCompte(17895, "Caroline", 200000m, 1000);
 
 
+            CompteBancaire compteSup = b1.CompteSup();
+            Console.WriteLine($"Le Compte avec le solde le plus élever:\n{compteSup}");
+
+
+            Console.WriteLine("Trouvez le compte")
+            CompteBancaire compteTrouve = b1.RendCompte(15515);
+            Console.WriteLine(compteTrouve == null ? "Aucun compte trouvé." : compteTrouve.ToString());
 
 
             Console.ReadLine();
