6673690bc6a0dd2acabb949a26b9b8feadfe77e3
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-06-04 12:17:15 +0200

    upload

---
+++ ExoPOOexo/ClassLibraryDe/De.cs
@@ -0,0 +1,61 @@
+﻿namespace ClassLibraryDe
+{
+    public class De
+    {
+        private int numeroFace;
+        private readonly int nbFaceMax;
+
+        public int NumeroFace { get => numeroFace;}
+
+
+        public De(int nbFaceMax)
+        {
+
+            if (nbFaceMax < 1)
+            {
+                throw new ArgumentException("Le nombre de faces doit être supérieur à 0.");
+            }
+
+            this.nbFaceMax = nbFaceMax;
+            this.numeroFace = 1;
+
+        }
+
+        public void Jeter()
+        {
+            Random de = new Random();
+            this.numeroFace = de.Next(1, this.nbFaceMax + 1);
+        }
+
+        public string ToString()
+        {
+            return this.numeroFace.ToString();
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ExoPOOexo/Jeu421/Program.cs
@@ -0,0 +1,29 @@
+﻿using ClassLibraryDe;
+using System.Xml.Linq;
+
+namespace Jeu421
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            De de1 = new De(6);
+            De de2 = new De(6);
+            De de3= new De(6);
+
+
+            de1.Jeter();
+            de2.Jeter();
+            de3.Jeter();
+
+
+
+
+            Console.WriteLine("Valeur du dé après le lancer: " + de1.ToString());
+            Console.WriteLine("Valeur du dé après le lancer: " + de2.ToString());
+            Console.WriteLine("Valeur du dé après le lancer: " + de3.ToString());
+
+            
+        }
+    }
+}
