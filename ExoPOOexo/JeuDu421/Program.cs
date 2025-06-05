b092a946501ac43a0431a4fbb8fc718ea84b34c9
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-06-05 16:40:14 +0200

    uplod

---
+++ ExoPOOexo/JeuDu421/Program.cs
@@ -0,0 +1,13 @@
+﻿using ClassLibraryManche;
+using ClassLibraryDe;
+
+namespace JeuDu421
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manche m = new Manche();
+        }
+    }
+}
